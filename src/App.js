4cbc15c194195e5c70574310ba3886dfeb14fc0f
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-03-05 11:06:39 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -5,21 +5,33 @@ import userData from './MOCK_DATA.json';
 import User from './component/User/User';
 
 function App() {
-  const [users,setUsers]= useState([])
-  useEffect(() =>{
+  const [users, setUsers] = useState([])
+  const [connect, setConnect] = useState([]);
+  useEffect(() => {
     setUsers(userData);
-  },[])
+  }, [])
+
+  const clickToConnect = (name) => {
+    setConnect([...connect, name])
+    window.scrollTo(0, 0)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Number of Users: {users.length}</h1>
         <h5>Users Added: </h5>
+        <ul>
+          {
+            connect.map(m =><li key={m}>{m}</li>)
+          }
+        </ul>
         <div className="users">
-        {
-          users.map(user=><User user={user}></User>)
-        }
+          {
+            users.map(user => <User user={user} clickToConnect={clickToConnect} key={user.id}></User>)
+          }
         </div>
-        
+
       </header>
     </div>
   );
--- src/component/User/User.js
+++ src/component/User/User.js
@@ -1,31 +1,37 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './User.css';
 
+
 const User = (props) => {
-      console.log(props)
       const { id, username, email, first_name, last_name, university, department, job_title, gender, image } = props.user;
+      const full_name = first_name + " " + last_name;
+      const clickToConnect = props.clickToConnect;
+      const [ID, setID]= useState();
+      const showId =()=>{setID(id)};
       return (
             <div className="card">
                   <div>
-                  <div className="image">
-                        <img src={image} alt="" />
-                  </div>
-                  <div className="details">
-                  <h2>{first_name} {last_name}</h2>
-                        <p>
-                              <br />
+                        <div className="image">
+                              <img src={image} alt="" />
+                        </div>
+                        <div className="details">
+                              <h2>{full_name}</h2>
+                              <p>
+                                    <br />
                               University: {university}
-                              <br />
-                              <br/>{id}
+                                    <br />
+                                    <br />Id: {ID}
+                                    <br />
                               Department: {department}
-                              <br/>Username: {username}
-                              <br/>{email}
-                              <br/>Job: {job_title}
-                              <br/>{gender}
-                        </p>
+                                    <br />Username: {username}
+                                    <br />{email}
+                                    <br />Job: {job_title}
+                                    <br />{gender}
+                              </p>
+                        </div>
                   </div>
-                  </div>
-                  <button onClick={()=>window.scrollTo(0, 0)}>Click to Connect</button>
+                  <button onClick={showId}>Click to get Id</button>
+                  <button onClick={() =>{clickToConnect(full_name)} }>Click to Connect</button>
             </div>
       );
 };
